8c4340f8dc7d4e4f0f95044d6d7d47a1a0f5124b
Jason Morse <jasonmo@microsoft.com>
2025-01-02 15:26:41 -0800

    update rnx-kit packages to current, add ts server package

--- lage.config.js
+++ lage.config.js
@@ -3,6 +3,9 @@ module.exports = {
   pipeline: {
     ['build-tools']: ['^build-tools'],
     build: ['build-tools', '^build'],
+    ['ts-emit']: ['build-tools', '^ts-emit'],
+    ['ts-check']: ['build-tools', 'ts-emit'],
+    ['build-new']: ['ts-emit', 'ts-check'],
     buildci: ['build', 'test', 'depcheck'],
     bundle: ['build-tools', 'build'],
     clean: [],
